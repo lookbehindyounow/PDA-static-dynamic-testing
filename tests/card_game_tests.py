565d429bd6ae838a4e46749abc5cf91ceff522d1
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-22 11:52:10 +0100

    fixed syntax errors in card game that stopped it from being able to run & tested ace check method

--- tests/card_game_tests.py
+++ tests/card_game_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from src.card import Card
-# from src.card_game import CardGame
+from src.card_game import CardGame
 
 class TestCardGame(unittest.TestCase):
     def setUp(self):
@@ -9,7 +9,7 @@ class TestCardGame(unittest.TestCase):
         self.cards.append([Card("H",i) for i in range(1,14)])
         self.cards.append([Card("C",i) for i in range(1,14)])
         self.cards.append([Card("D",i) for i in range(1,14)])
-        # self.game=CardGame()
+        self.game=CardGame()
     
     def test_cards_have_attributes(self):
         self.assertEqual(self.cards[0][0].value,1)
@@ -17,7 +17,6 @@ class TestCardGame(unittest.TestCase):
         self.assertEqual(self.cards[3][12].value,13)
         self.assertEqual(self.cards[3][12].suit,"D")
     
-    @unittest.skip("")
     def test_check_for_ace(self):
         self.assertTrue(self.game.check_for_ace(self.cards[0][0]))
         self.assertFalse(self.game.check_for_ace(self.cards[0][1]))

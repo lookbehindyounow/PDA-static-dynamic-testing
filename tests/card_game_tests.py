0772bed9c87dd66aa7eca496aa81102033301b6e
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-22 11:37:30 +0100

    tested card attributes

--- tests/card_game_tests.py
+++ tests/card_game_tests.py
@@ -1,12 +1,18 @@
 import unittest
 from src.card import Card
-from src.card_game import CardGame
+# from src.card_game import CardGame
 
 class TestCardGame(unittest.TestCase):
     def setUp(self):
         self.cards=[]
-        self.cards.append([Card("S",i) for i in range(1,13)])
-        self.cards.append([Card("H",i) for i in range(1,13)])
-        self.cards.append([Card("C",i) for i in range(1,13)])
-        self.cards.append([Card("D",i) for i in range(1,13)])
-        self.game=CardGame()
+        self.cards.append([Card("S",i) for i in range(1,14)])
+        self.cards.append([Card("H",i) for i in range(1,14)])
+        self.cards.append([Card("C",i) for i in range(1,14)])
+        self.cards.append([Card("D",i) for i in range(1,14)])
+        # self.game=CardGame()
+    
+    def test_cards_have_attributes(self):
+        self.assertEqual(self.cards[0][0].value,1)
+        self.assertEqual(self.cards[0][0].suit,"S")
+        self.assertEqual(self.cards[3][12].value,13)
+        self.assertEqual(self.cards[3][12].suit,"D")
